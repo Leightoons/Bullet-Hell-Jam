53442eea778ee26fc1560f013c789d0b45ec8487
Leightoons <65992785+Leightoons@users.noreply.github.com>
2021-04-10 22:20:22 -0400

    Baby's first coroutine

--- Bullet Hell Jam/Assets/Scripts/TestScript.cs
+++ Bullet Hell Jam/Assets/Scripts/TestScript.cs
@@ -9,19 +9,32 @@ public class TestScript : MonoBehaviour {
     void Start() {
         _pool = GetComponent<ObjectPool<Bullet>>();
 
-        //Debug.Log("_bool is " + SingletonScriptableObject.Instance._bool);
-        //Debug.Log("_float is " + SingletonScriptableObject.Instance._float);
-        //Debug.Log("_int is " + SingletonScriptableObject.Instance._int);
-
+        StartCoroutine(TestCoroutine());
     }
+
+    IEnumerator TestCoroutine() {
+        Bullet[] bList = Bullet.FindObjectsOfType<Bullet>();
+        while (bList.Length + 1 < 5) {
+            bList = Bullet.FindObjectsOfType<Bullet>();
+            NewBullet();
+            print("Added new bullet; " + (5 - (bList.Length + 1)) + " until 5 total");
+            yield return new WaitForSeconds(1.5f);
+        }
+        print("Coroutine finished");
+        yield return null;
+    }
+
+    void NewBullet() {
+        Bullet _new = _pool.Get();
+        _new.gameObject.SetActive(true);
+        _new.transform.position = new Vector3(Random.Range(-7.5f, 7.5f), Random.Range(-3.5f, 3.5f), 0);
+    }
+
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            //
-            Bullet _new = _pool.Get();
-            _new.gameObject.SetActive(true);
-            _new.transform.position = new Vector3(Random.Range(-7.5f,7.5f),Random.Range(-3.5f,3.5f) , 0);
+            NewBullet();
         }
     }
 }
